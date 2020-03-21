67636a54592ae3ef9f8f7ea52ae82471c5744a81
joshuaweiss2020 <3373182@qq.com>
2020-03-22 00:28:13 +0800

    20200321

--- p2pClient.py
+++ p2pClient.py
@@ -9,10 +9,10 @@ from tkinter import StringVar, IntVar
 
 # s = ServerProxy('http://106.13.113.252:9001')
 
-#URL = "http://106.13.113.252:9001"
+URL = "http://106.13.113.252:9001"
 
 
-URL = "http://127.0.0.1:2001"
+#URL = "http://127.0.0.1:2001"
 
 
 
